8701f64b46b71dbe4647ec7f68b3e254204ff4c1
rayanalim <rayan.peaceawad@gmail.com>
2024-04-21 21:39:18 +0000

    Simulate data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,64 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates GDP growth and energy consaumption for counttries from 2022 to 2012
+# Author: Rayan Awad Alim
+# Date: 21 April 2024
+# Contact: rayan.alim@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: N/A ;
+# Any other information needed? Data simulated here tries to model these two data sets https://data.worldbank.org/indicator/NY.GDP.MKTP.KD.ZG and https://ourworldindata.org/energy
 
 
 #### Workspace setup ####
+# Install Packages if not downloaded:
+install_if_missing <- function(package_name) {
+  if (!requireNamespace(package_name, quietly = TRUE)) {
+    install.packages(package_name)
+    suppressMessages(library(package_name, character.only = TRUE))
+  }
+}
+
+install_if_missing("dplyr")
+install_if_missing("ggplot2")
+install_if_missing("tidyverse")
+
+#Load Libraries
+library(dplyr)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(ggplot2)
 
-#### Simulate data ####
-# [...ADD CODE HERE...]
+# Defining some random countries fro simulation
+countries <- c("USA", "China", "India", "Australia", "Brazil")
+years <- 2012:2022
+
+# Creating a data frame with all combinations of country and year
+simulated_data <- expand.grid(Country = countries, Year = years)
+
+## Simulating the Total Energy Consumption (in Petajoules)
+
+# Assuming mean consumption and sd
+simulated_data$TotalEnergyConsumption <-
+  rnorm(n = nrow(data), mean = 5000, sd = 1000)
+
+## Simulate GDP Growth (annual %)
+
+# Assuming mean GDP growth is 3% and sd is 1%
+simulated_data$GDPGrowth <- rnorm(n = nrow(data), mean = 3, sd = 1)
+
+# View the first few rows of the dataset
+head(simulated_data)
 
 
-
+# Plotting Energy consumption over years for each Country
+library(ggplot2)
+ggplot(data,
+       aes(
+         x = Year,
+         y = TotalEnergyConsumption,
+         group = Country,
+         color = Country
+       )) +
+  geom_line() +
+  ggtitle("Total Energy Consumption over Years by Country") +
+  xlab("Year") +
+  ylab("Total Energy Consumption (PJ)") +
+  theme_minimal()
